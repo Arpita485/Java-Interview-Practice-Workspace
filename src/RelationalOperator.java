77e6d4adfd4a508a2db34ebf96540d7686370108
Arpita Sahu <arpitasahu485@gmail.com>
2026-08-04 22:26:25 +0530

    Relational Operator

---
+++ src/RelationalOperator.java
@@ -0,0 +1,21 @@
+public class RelationalOperator {
+
+    //Comparison
+
+    //return boolean value(true or false)
+    public static void main(String[] args){
+
+        int a= 5;
+        int b= 6;
+        int c=7;
+
+        System.out.println(a==b); //false
+        System.out.println(a!=b); //true
+        System.out.println(c>=a); //true
+        System.out.println(b<=c); //true
+
+    }
+
+    // = is assignment.
+    // == is comparison.
+}
