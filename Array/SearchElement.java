2ba831c98e511b5256f147b8cce6cdbf3dd204ca
Arpita Sahu <arpitasahu485@gmail.com>
2026-08-05 16:51:34 +0530

    Array

---
+++ Array/SearchElement.java
@@ -0,0 +1,13 @@
+public class SearchElement {
+    public static void main(String[] args) {
+        //search element
+        int b[] = {1, 9, 3, 0, 24};
+
+        for (int i : b) {
+            if (i == 0) {
+                System.out.println("Found");
+            }
+        }
+
+    }
+}
---
+++ BasicQues/ReverseNum.java
@@ -0,0 +1,2 @@
+public class ReverseNum {
+}
