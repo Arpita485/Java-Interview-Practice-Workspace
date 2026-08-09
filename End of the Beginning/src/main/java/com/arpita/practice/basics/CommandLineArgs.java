30ea0021f6c457b1b6e729e1d2c78610e6ba74e4
Arpita Sahu <arpitasahu485@gmail.com>
2026-08-09 17:05:09 +0530

    Command Line Arguments

---
+++ End of the Beginning/src/main/java/com/arpita/practice/basics/CommandLineArgs.java
@@ -0,0 +1,17 @@
+package com.arpita.practice.basics;
+
+public class CommandLineArgs {
+    public static void main(String[] abc) {
+        //abc- parameter from JVM
+
+        String[] args = {"Arpita", "Sahu"};
+        //args local variable inside method
+
+        abc[0] = "Arpita";
+        abc[1] = "Sahu";
+
+        System.out.println(abc[0]);
+        System.out.println(abc[1]);
+    }
+
+}
